05bf02df92a25ed18bb91c7254e52b3100392f4f
Heng Li <lh3@me.com>
2012-05-23 11:45:44 -0400

    added comments

--- bprope6.c
+++ bprope6.c
@@ -44,7 +44,7 @@ static inline void *mp_alloc(mempool_t *mp)
 }
 
 static int insert_to_leaf(uint8_t *p, int a, int x)
-{
+{ // insert $a after $x symbols in $p; IMPORTANT: the first 4 bytes of $p gives the length of the string
 #define MAX_RUNLEN 31
 #define _insert_after(_n, _s, _i, _b) if ((_i) + 1 != (_n)) memmove(_s+(_i)+2, _s+(_i)+1, (_n)-(_i)-1); _s[(_i)+1] = (_b); ++(_n)
 
@@ -98,19 +98,19 @@ static int insert_to_leaf(uint8_t *p, int a, int x)
 }
 
 typedef struct bpr_node_s {
-	struct bpr_node_s *p;
-	uint64_t l:54, n:9, is_bottom:1;
-	uint64_t c[6];
-} bpr_node_t;
+	struct bpr_node_s *p; // child; at the bottom level, $p points to a string with the first 4 bytes giving the number of runs (#runs)
+	uint64_t l:54, n:9, is_bottom:1; // $n and $is_bottom are only set for the first node in a bucket
+	uint64_t c[6]; // marginal counts
+} node_t;
 
 struct bprope6_s {
 	int max_nodes, max_runs; // both MUST BE even numbers
-	uint64_t c[6];
-	bpr_node_t *root;
+	uint64_t c[6]; // marginal counts
+	node_t *root;
 	mempool_t *node, *leaf;
 };
 
-static void print_node(const bpr_node_t *p)
+static void print_node(const node_t *p) // recursively print the B+ rope in the Newick format
 {
 	if (p->is_bottom) {
 		int i, j, k;
@@ -137,69 +137,67 @@ static void print_node(const bpr_node_t *p)
 
 void bpr_print(const bprope6_t *rope) { print_node(rope->root); putchar('\n'); }
 
-static inline bpr_node_t *split_node(bprope6_t *rope, bpr_node_t *u, bpr_node_t *v)
-{ // $u: the first node in the bucket; $v: whose child to be split; 
+static inline node_t *split_node(bprope6_t *rope, node_t *u, node_t *v)
+{ // split $v's child. $u is the first node in the bucket. $v and $u are in the same bucket. IMPORTANT: there is always enough room in $u
 	int j, i = v - u;
-	bpr_node_t *w; // this is the uncle
-	if (u == 0) { // only happens at the root
+	node_t *w; // $w is the sibling of $v
+	if (u == 0) { // only happens at the root; add a new root
 		u = v = mp_alloc(rope->node);
-		v->n = 1; v->p = rope->root;
+		v->n = 1; v->p = rope->root; // the new root has the old root as the only child
 		memcpy(v->c, rope->c, 48);
 		for (j = 0; j < 6; ++j) v->l += v->c[j];
 		rope->root = v;
 	}
-	if (i != u->n - 1)
-		memmove(v + 2, v + 1, sizeof(bpr_node_t) * (u->n - i - 1));
-	++u->n;
-	w = v + 1;
-	memset(w, 0, sizeof(bpr_node_t));
+	if (i != u->n - 1) // then make room for a new node
+		memmove(v + 2, v + 1, sizeof(node_t) * (u->n - i - 1));
+	++u->n; w = v + 1;
+	memset(w, 0, sizeof(node_t));
 	w->p = mp_alloc(u->is_bottom? rope->leaf : rope->node);
-	if (u->is_bottom) {
+	if (u->is_bottom) { // we are at the bottom level; $v->p is a string instead of a node
 		uint8_t *p = (uint8_t*)v->p, *q = (uint8_t*)w->p;
-		int32_t *np = (int32_t*)p, *nq = (int32_t*)q;
-		*nq = *np - (rope->max_runs>>1); *np -= *nq;
+		int32_t *np = (int32_t*)p, *nq = (int32_t*)q; // the first 4 bytes give the number of runs (#runs)
+		*nq = *np - (rope->max_runs>>1); *np -= *nq; // compute the new #runs
 		memcpy(q + 4, p + 4 + *np, *nq);
-		for (i = 0, q += 4; i < *nq; ++i)
+		for (i = 0, q += 4; i < *nq; ++i) // compute the count arrays in $w, from $q
 			w->c[q[i]&7] += q[i]>>3;
-	} else {
-		bpr_node_t *p = v->p, *q = w->p; // $p and $q are cousins
+	} else { // $v->p is a node, not a string
+		node_t *p = v->p, *q = w->p; // $v and $w are siblings and thus $p and $q are cousins
 		p->n -= rope->max_nodes>>1;
-		memcpy(q, p + p->n, sizeof(bpr_node_t) * (rope->max_nodes>>1));
-		q->n = rope->max_nodes>>1;
+		memcpy(q, p + p->n, sizeof(node_t) * (rope->max_nodes>>1));
+		q->n = rope->max_nodes>>1; // NB: this line must below memcpy() as $q->n and $q->is_bottom are modified by memcpy()
 		q->is_bottom = p->is_bottom;
 		for (i = 0; i < q->n; ++i)
 			for (j = 0; j < 6; ++j)
 				w->c[j] += q[i].c[j];
 	}
-	for (j = 0; j < 6; ++j)
+	for (j = 0; j < 6; ++j) // compute $w->l and update $v->c
 		w->l += w->c[j], v->c[j] -= w->c[j];
-	v->l -= w->l;
+	v->l -= w->l; // update $v->c
 	return v;
 }
 
 int64_t bpr_insert_symbol(bprope6_t *rope, int a, int64_t x)
-{
-	bpr_node_t *u = 0, *v = 0, *p = rope->root;
+{ // insert $a after $x symbols in $rope and the returns the position of the next insertion
+	node_t *u = 0, *v = 0, *p = rope->root; // $v is the parent of $p; $u and $v are at the same level and $u is the first node in the bucket
 	int64_t y = 0, z;
 	int i;
 	for (i = 0, z = 0; i < a; ++i) z += rope->c[i];
-	do {
+	do { // top-down update. Searching and node splitting are done together in one pass.
 		if (p->n == rope->max_nodes) { // node is full; split
-			v = split_node(rope, u, v);
-			if (y + v->l < x)
+			v = split_node(rope, u, v); // $v points to the parent of $p; when a new root is added, $v points to the root
+			if (y + v->l < x) // if $v is not long enough after the split, we need to move both $p and its parent $v
 				y += v->l, z += v->c[a], ++v, p = v->p;
 		}
-		for (u = p; y + p->l < x; ++p) y += p->l, z += p->c[a];
+		for (u = p; y + p->l < x; ++p) y += p->l, z += p->c[a]; // find the right node to descend
 		assert(p - u < u->n);
-		if (v) ++v->c[a], ++v->l; // we should not change p->c[a] because when this will lead to problems when p's child is split
-		v = p; p = p->p;
+		if (v) ++v->c[a], ++v->l; // we should not change p->c[a] because this may cause troubles when p's child is split
+		v = p; p = p->p; // descend
 	} while (!u->is_bottom);
 	++v->c[a]; ++v->l;
-	++rope->c[a];
+	++rope->c[a]; // $rope->c should be updated after the loop as adding a new root needs the old $rope->c counts
 	z += insert_to_leaf((uint8_t*)p, a, x - y) + 1;
 	if (*(uint32_t*)p + 2 > rope->max_runs)
 		split_node(rope, u, v);
-//	printf("%c,%lld\t", "$ACGTN"[a], x); bpr_print(rope); fflush(stdout);
 	return z;
 }
 
@@ -217,9 +215,9 @@ bprope6_t *bpr_init(int max_nodes, int max_runs)
 	rope = calloc(1, sizeof(bprope6_t));
 	if (max_runs < 8) max_runs = 8;
 	rope->max_nodes= (max_nodes+ 1)>>1<<1;
-	rope->max_runs = ((max_runs + 1)>>1<<1) - 4;
-	rope->node = mp_init(sizeof(bpr_node_t) * rope->max_nodes);
-	rope->leaf = mp_init(rope->max_runs + 4);
+	rope->max_runs = ((max_runs + 1)>>1<<1) - 4; // -4 to make room for the 4-byte integer keeping #runs
+	rope->node = mp_init(sizeof(node_t) * rope->max_nodes);
+	rope->leaf = mp_init(rope->max_runs + 4); // +4 to include the number of runs
 	rope->root = mp_alloc(rope->node);
 	rope->root->n = 1;
 	rope->root->is_bottom = 1;
@@ -234,12 +232,10 @@ void bpr_destroy(bprope6_t *rope)
 	free(rope);
 }
 
-#define MAX_HEIGHT 80
-
 struct bpriter_s {
 	const bprope6_t *rope;
-	const bpr_node_t *pa[MAX_HEIGHT];
-	int k, ia[MAX_HEIGHT];
+	const node_t *pa[80];
+	int k, ia[80];
 };
 
 bpriter_t *bpr_iter_init(const bprope6_t *rope)
@@ -255,6 +251,7 @@ bpriter_t *bpr_iter_init(const bprope6_t *rope)
 const uint8_t *bpr_iter_next(bpriter_t *i, int *n)
 {
 	const uint8_t *ret;
+	assert(i->k < 80); // a B+ tree should not be that tall
 	if (i->k < 0) return 0;
 	*n = *(int32_t*)i->pa[i->k][i->ia[i->k]].p;
 	ret = (uint8_t*)i->pa[i->k][i->ia[i->k]].p + 4;
