67ccee65fd274917ce2f536b787fdfee49a5e266
Heng Li <lh3@me.com>
2012-06-07 11:42:07 -0400

    improve CLI

--- main-misc.c
+++ main-misc.c
@@ -8,7 +8,7 @@
 #include "kseq.h"
 KSEQ_INIT(gzFile, gzread)
 
-unsigned char seq_nt6_table[128] = {
+static unsigned char seq_nt6_table[128] = {
     0, 5, 5, 5,  5, 5, 5, 5,  5, 5, 5, 5,  5, 5, 5, 5,
     5, 5, 5, 5,  5, 5, 5, 5,  5, 5, 5, 5,  5, 5, 5, 5,
     5, 5, 5, 5,  5, 5, 5, 5,  5, 5, 5, 5,  5, 5, 5, 5,
@@ -19,26 +19,14 @@ unsigned char seq_nt6_table[128] = {
     5, 5, 5, 5,  4, 5, 5, 5,  5, 5, 5, 5,  5, 5, 5, 5
 };
 
-void seq_char2nt6(int l, unsigned char *s)
-{
-	int i;
-	for (i = 0; i < l; ++i)
-		s[i] = s[i] < 128? seq_nt6_table[s[i]] : 5;
-}
-
-void seq_revcomp6(int l, unsigned char *s)
-{
-	int i;
-	for (i = 0; i < l>>1; ++i) {
-		int tmp = s[l-1-i];
-		tmp = (tmp >= 1 && tmp <= 4)? 5 - tmp : tmp;
-		s[l-1-i] = (s[i] >= 1 && s[i] <= 4)? 5 - s[i] : s[i];
-		s[i] = tmp;
-	}
-	if (l&1) s[i] = (s[i] >= 1 && s[i] <= 4)? 5 - s[i] : s[i];
-}
-
 enum algo_e { BPR, RBR, BCR };
+
+#define FLAG_FOR 0x1
+#define FLAG_REV 0x2
+#define FLAG_ODD 0x4
+#define FLAG_BIN 0x8
+#define FLAG_TREE 0x10
+#define FLAG_THR 0x20
 
 int main(int argc, char *argv[])
 {
@@ -46,86 +34,117 @@ int main(int argc, char *argv[])
 	bprope6_t *bpr = 0;
 	bcr_t *bcr = 0;
 	gzFile fp;
+	FILE *out = stdout;
 	kseq_t *ks;
 	enum algo_e algo = BPR;
-	int c, i, j, for_only = 0, print_rope = 0, max_runs = 512, max_nodes = 64, n_threads = 1;
-	int64_t cnt = 0;
-	const uint8_t *s;
+	int c, i, max_runs = 512, max_nodes = 64;
+	int flag = FLAG_FOR | FLAG_REV | FLAG_ODD;
 
-	while ((c = getopt(argc, argv, "Tfr:n:t:a:")) >= 0)
+	while ((c = getopt(argc, argv, "TFROo:r:n:t:a:")) >= 0)
 		if (c == 'a') {
 			if (strcmp(optarg, "bpr") == 0) algo = BPR;
 			else if (strcmp(optarg, "rbr") == 0) algo = RBR;
 			else if (strcmp(optarg, "bcr") == 0) algo = BCR;
 			else fprintf(stderr, "[W::%s] available algorithms: bpr, rbr or bcr; default to bpr\n", __func__);
-		} else if (c == 'f') for_only = 1;
-		else if (c == 'T') print_rope = 1;
+		} else if (c == 'o') out = fopen(optarg, "wb");
+		else if (c == 'F') flag &= ~FLAG_FOR;
+		else if (c == 'R') flag &= ~FLAG_REV;
+		else if (c == 'T') flag |= FLAG_TREE;
+		else if (c == 'b') flag |= FLAG_BIN;
+		else if (c == 'O') flag &= ~FLAG_ODD;
 		else if (c == 'r') max_runs = atoi(optarg);
 		else if (c == 'n') max_nodes= atoi(optarg);
-		else if (c == 't') n_threads = atoi(optarg);
+		else if (c == 't') flag |= FLAG_THR;
+
 	if (optind == argc) {
-		fprintf(stderr, "Usage: ropebwt [-fT] [-r maxRuns=%d] [-n maxNodes=%d] <in.fq.gz>\n", max_runs, max_nodes);
+		fprintf(stderr, "\n");
+		fprintf(stderr, "Usage:   ropebwt [options] <in.fq.gz>\n\n");
+		fprintf(stderr, "Options: -a STR     algorithm: bpr, rbr or bcr [bpr]\n");
+		fprintf(stderr, "         -r INT     max number of runs in leaves (bpr and rbr only) [%d]\n", max_runs);
+		fprintf(stderr, "         -n INT     max number children per internal node (bpr only) [%d]\n", max_nodes);
+		fprintf(stderr, "         -o FILE    output file [stdout]\n");
+		fprintf(stderr, "         -t         enable threading (bcr only)\n");
+		fprintf(stderr, "         -F         skip forward strand\n");
+		fprintf(stderr, "         -R         skip reverse strand\n");
+		fprintf(stderr, "         -O         suppress end trimming when forward==reverse\n");
+		fprintf(stderr, "         -T         print the tree stdout (bpr and rbr only)\n\n");
 		return 1;
 	}
 
-	if (algo == BPR) bpr = bpr_init(max_nodes, max_runs);
+	if (algo == BCR) {
+		bcr = bcr_init(flag&FLAG_THR);
+		fprintf(stderr, "[W::%s] with bcr, all sequences must be of equal length; an ambiguous base converted to a random base\n", __func__);
+	} else if (algo == BPR) bpr = bpr_init(max_nodes, max_runs);
 	else if (algo == RBR) rbr = rbr_init(max_runs);
-	else if (algo == BCR) bcr = bcr_init(n_threads > 1);
 	fp = gzopen(argv[optind], "rb");
 	ks = kseq_init(fp);
 	while (kseq_read(ks) >= 0) {
+		int l = ks->seq.l;
 		uint8_t *s = (uint8_t*)ks->seq.s;
-		++cnt;
-		seq_char2nt6(ks->seq.l, s);
-		if (rbr) rbr_insert_string(rbr, ks->seq.l, s);
-		if (bpr) bpr_insert_string(bpr, ks->seq.l, s);
-		if (bcr) bcr_append(bcr, ks->seq.l, s);
-		if (!for_only) {
-			++cnt;
-			seq_revcomp6(ks->seq.l, s);
+		for (i = 0; i < l; ++i)
+			s[i] = s[i] < 128? seq_nt6_table[s[i]] : 5;
+		if (algo == BCR)
+			for (i = 0; i < l; ++i)
+				if (s[i] == 5) s[i] = (lrand48()&3) + 1;
+		if ((flag & FLAG_ODD) && (l&1) == 0) { // then check reverse complement
+			for (i = 0; i < l>>1; ++i) // is the reverse complement is identical to itself?
+				if (s[i] + s[l-1-i] != 5) break;
+			if (i == l>>1) --l; // if so, trim 1bp from the end
+		}
+		if (flag & FLAG_FOR) {
+			if (rbr) rbr_insert_string(rbr, ks->seq.l, s);
+			if (bpr) bpr_insert_string(bpr, ks->seq.l, s);
+			if (bcr) bcr_append(bcr, ks->seq.l, s);
+		}
+		if (flag & FLAG_REV) {
+			for (i = 0; i < l>>1; ++i) {
+				int tmp = s[l-1-i];
+				tmp = (tmp >= 1 && tmp <= 4)? 5 - tmp : tmp;
+				s[l-1-i] = (s[i] >= 1 && s[i] <= 4)? 5 - s[i] : s[i];
+				s[i] = tmp;
+			}
+			if (l&1) s[i] = (s[i] >= 1 && s[i] <= 4)? 5 - s[i] : s[i];
 			if (rbr) rbr_insert_string(rbr, ks->seq.l, s);
 			if (bpr) bpr_insert_string(bpr, ks->seq.l, s);
 			if (bcr) bcr_append(bcr, ks->seq.l, s);
 		}
 	}
-	fprintf(stderr, "[M::%s] processed %ld sequences\n", __func__, (long)cnt);
 	kseq_destroy(ks);
 	gzclose(fp);
 
+#define print_bwt(itr_t, itr_set, itr_next_f, is_bin, fp) do { \
+		itr_t *itr; \
+		const uint8_t *s; \
+		int i, j, l; \
+		itr = (itr_set); \
+		if (is_bin) { \
+			while ((s = itr_next_f(itr, &l)) != 0) \
+				fwrite(s, 1, l, fp); \
+		} else { \
+			while ((s = itr_next_f(itr, &l)) != 0) \
+				for (i = 0; i < l; ++i) \
+					for (j = 0; j < s[i]>>3; ++j) \
+						fputc("$ACGTN"[s[i]&7], fp); \
+			fputc('\n', fp); \
+		} \
+		free(itr); \
+	} while (0)
+
 	if (rbr) {
-		rbriter_t *iter;
-		iter = rbr_iter_init(rbr);
-		while ((s = rbr_iter_next(iter, &c)) != 0)
-			for (i = 0; i < c; ++i)
-				for (j = 0; j < s[i]>>3; ++j)
-					putchar("$ACGTN"[s[i]&7]);
-		putchar('\n');
-		if (print_rope) rbr_print(rbr);
-		free(iter);
+		print_bwt(rbriter_t, rbr_iter_init(rbr), rbr_iter_next, flag&FLAG_BIN, out);
+		if (flag&FLAG_TREE) rbr_print(rbr);
 		rbr_destroy(rbr);
 	}
 	if (bpr) {
-		bpriter_t *iter = bpr_iter_init(bpr);
-		while ((s = bpr_iter_next(iter, &c)) != 0)
-			for (i = 0; i < c; ++i)
-				for (j = 0; j < s[i]>>3; ++j)
-					putchar("$ACGTN"[s[i]&7]);
-		putchar('\n');
-		if (print_rope) bpr_print(bpr);
-		free(iter);
+		print_bwt(bpriter_t, bpr_iter_init(bpr), bpr_iter_next, flag&FLAG_BIN, out);
+		if (flag&FLAG_TREE) bpr_print(bpr);
 		bpr_destroy(bpr);
 	}
 	if (bcr) {
-		bcritr_t *itr;
 		bcr_build(bcr);
-		itr = bcr_itr_init(bcr);
-		while ((s = bcr_itr_next(itr, &c)) != 0)
-			for (i = 0; i < c; ++i)
-				for (j = 0; j < s[i]>>3; ++j)
-					putchar("$ACGTN"[s[i]&7]);
-		putchar('\n');
-		free(itr);
+		print_bwt(bcritr_t, bcr_itr_init(bcr), bcr_itr_next, flag&FLAG_BIN, out);
 		bcr_destroy(bcr);
 	}
+	fclose(out);
 	return 0;
 }
