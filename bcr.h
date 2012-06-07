14a1d1a4f17196ca0bcda3a5e5b41e9d8f61d829
Heng Li <lh3@me.com>
2012-06-07 11:49:28 -0400

    forgot to add this

---
+++ bcr.h
@@ -0,0 +1,28 @@
+#ifndef BCR_H
+#define BCR_H
+
+#include <stdint.h>
+
+struct bcr_s;
+typedef struct bcr_s bcr_t;
+
+struct bcritr_s;
+typedef struct bcritr_s bcritr_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	bcr_t *bcr_init(int is_thr);
+	void bcr_destroy(bcr_t *b);
+	void bcr_append(bcr_t *b, int len, uint8_t *seq);
+	void bcr_build(bcr_t *b);
+
+	bcritr_t *bcr_itr_init(const bcr_t *b);
+	const uint8_t *bcr_itr_next(bcritr_t *itr, int *l);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
