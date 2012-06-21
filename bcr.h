994d086fd0b111939f90e73d637f6015e4f4a805
Heng Li <lh3@me.com>
2012-06-21 17:24:10 -0400

    added licensing information

--- bcr.h
+++ bcr.h
@@ -1,3 +1,28 @@
+/* The MIT License
+
+   Copyright (c) 2012 Heng Li <lh3@me.com>
+
+   Permission is hereby granted, free of charge, to any person obtaining
+   a copy of this software and associated documentation files (the
+   "Software"), to deal in the Software without restriction, including
+   without limitation the rights to use, copy, modify, merge, publish,
+   distribute, sublicense, and/or sell copies of the Software, and to
+   permit persons to whom the Software is furnished to do so, subject to
+   the following conditions:
+
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
+
 #ifndef BCR_H
 #define BCR_H
 
@@ -15,7 +40,7 @@ extern int bcr_verbose;
 extern "C" {
 #endif
 
-	bcr_t *bcr_init(int is_thr, const char *tmpfn);
+	bcr_t *bcr_init(int is_threaded, const char *tmpfn);
 	void bcr_destroy(bcr_t *b);
 	void bcr_append(bcr_t *b, int len, uint8_t *seq);
 	void bcr_build(bcr_t *b);
