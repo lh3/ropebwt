ef282fb52da73a6eb235552cd817b4c341202016
Heng Li <lh3@me.com>
2012-09-08 01:15:53 -0400

    speed up bcr-demo at the cost of more RAM

--- bcr-demo.c
+++ bcr-demo.c
@@ -6,54 +6,6 @@ typedef struct {
 	uint64_t u, v;
 } pair64_t;
 
-#define rstype_t pair64_t
-#define rskey(x) ((x).u)
-
-#define RS_MIN_SIZE 64
-
-typedef struct {
-	rstype_t *b, *e;
-} rsbucket_t;
-
-void rs_sort(rstype_t *beg, rstype_t *end, int n_bits, int s) // integer radix sort
-{
-	rstype_t *i;
-	int size = 1<<n_bits, m = size - 1;
-	rsbucket_t *k, b[size], *be = b + size;
-
-	for (k = b; k != be; ++k) k->b = k->e = beg;
-	for (i = beg; i != end; ++i) ++b[rskey(*i)>>s&m].e; // count radix
-	for (k = b + 1; k != be; ++k) // set start and end of each bucket
-		k->e += (k-1)->e - beg, k->b = (k-1)->e;
-	for (k = b; k != be;) { // in-place classification based on radix
-		if (k->b != k->e) { // the bucket is not full
-			rsbucket_t *l;
-			if ((l = b + (rskey(*k->b)>>s&m)) != k) { // destination different
-				rstype_t tmp = *k->b, swap;
-				do { // swap until we find an element in $k
-					swap = tmp; tmp = *l->b; *l->b++ = swap;
-					l = b + (rskey(tmp)>>s&m);
-				} while (l != k);
-				*k->b++ = tmp;
-			} else ++k->b;
-		} else ++k;
-	}
-	for (b->b = beg, k = b + 1; k != be; ++k) k->b = (k-1)->e; // reset k->b
-	if (s) { // if $s is non-zero, we need to sort buckets
-		s = s > n_bits? s - n_bits : 0;
-		for (k = b; k != be; ++k)
-			if (k->e - k->b > RS_MIN_SIZE) rs_sort(k->b, k->e, n_bits, s);
-			else if (k->e - k->b > 1) // then use an insertion sort
-				for (i = k->b + 1; i < k->e; ++i)
-					if (rskey(*i) < rskey(*(i - 1))) {
-						rstype_t *j, tmp = *i;
-						for (j = i; j > k->b && rskey(tmp) < rskey(*(j-1)); --j)
-							*j = *(j - 1);
-						*j = tmp;
-					}
-	}
-}
-
 /**
  * Append $T to existing BWT $B.
  *
@@ -92,25 +44,28 @@ uint8_t *bcr_lite(long Blen, uint8_t *B, long Tlen, const uint8_t *T)
 	B = B0 = B + Tlen;
 	// core loop
 	for (i = 0, n0 = n; n0; ++i) {
-		long l, pre, ac[256], mc[256];
-		for (c = 0; c != 256; ++c) mc[c] = 0;
+		long l, pre, ac[256], mc[256], mc2[256];
+		pair64_t *b[256], *aa;
+		for (c = 0; c != 256; ++c) mc[c] = mc2[c] = 0;
 		end = B0 + Blen; Blen += n0; B -= n0;
-		for (n = k = 0, p = B0, q = B, pre = -1L; k < n0; ++k) {
+		for (n = k = 0, p = B0, q = B, pre = 0; k < n0; ++k) {
 			pair64_t *u = &a[k];
 			c = P[(u->v>>8) + 1] - 2 - i >= P[u->v>>8]? *(P[(u->v>>8) + 1] - 2 - i) : 0; // symbol to insert
 			u->v = (u->v&~0xffULL) | c;
-			if (u->u - pre - 1) // then copy ($u->u - $pre - 1) symbols from B0 to B
-				for (l = 0; l < u->u - pre - 1; ++l)
-					++mc[*p], *q++ = *p++;
+			for (l = 0; l != u->u - pre; ++l) // copy ($u->u - $pre - 1) symbols from B0 to B
+				++mc[*p], *q++ = *p++; // $mc: marginal counts of all processed symbols
 			*q++ = c;
-			pre = u->u; u->u = mc[c]++;
-			if (c) a[n++] = a[k];
+			pre = u->u + 1; u->u = mc[c]++;
+			if (c) a[n++] = a[k], ++mc2[c]; // $mc2: marginal counts of the current column
 		}
 		while (p < end) ++mc[*p], *q++ = *p++; // copy the rest of $B0 to $B
 		for (c = 1, ac[0] = 0; c != 256; ++c) ac[c] = ac[c-1] + mc[c-1]; // accumulative count
-		for (k = 0; k < n; ++k) a[k].u += ac[a[k].v&0xff] + n; // compute the absolute positions
-		for (k = Blen, c = 0; k; k >>= 1, ++c); // find how many bits needed for radix sort
-		if (n) rs_sort(a, a + n, 8, c > 7? c - 7 : 0); // radix sort
+		for (k = 0; k < n; ++k) a[k].u += ac[a[k].v&0xff] + n; // compute positions for the next round
+		// stable sort by ($a[k].v&0xff); also possible with an in-place non-stable radix sort, which is slower
+		aa = malloc(sizeof(pair64_t) * n);
+		for (c = 1, b[0] = aa; c != 256; ++c) b[c] = b[c-1] + mc2[c-1];
+		for (k = 0; k < n; ++k) *b[a[k].v&0xff]++ = a[k]; // this works because $a is already partially sorted
+		free(a); a = aa; // $aa now becomes $a
 		B0 = B; n0 = n;
 	}
 	free(P); free(a);
